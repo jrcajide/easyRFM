d8c6c543c8268e46943a6f3c50c6e696763e9c9e
hoxo-m <hoxo.smile@gmail.com>
2014-12-07 17:10:27 +0900

    [add] rfm_compute_breaks()

---
+++ R/rfm_compute_breaks.R
@@ -0,0 +1,13 @@
+#'Compute breaks
+#'
+#'@export
+rfm_compute_breaks <- function(values, break_num=5) {
+  step = 1 / break_num
+  breaks <- quantile(values, probs = seq(0, 1, step))
+  breaks <- unname(breaks)
+  if(length(breaks) == length(unique(breaks))) return(breaks)
+  min_value <- min(values)
+  next_values <- Filter(function(x) x != min_value, values)
+  next_breaks <- compute_breaks(next_values, break_num = break_num - 1)
+  unname(c(min_value - 1, next_breaks))
+}
