c960e19ec5392fb388062b96fd8f4e06af555825
hoxo-m <hoxo.smile@gmail.com>
2014-12-08 18:49:02 +0900

    [modify] data.frame -> data_frame

--- R/rfm_generate_data.R
+++ R/rfm_generate_data.R
@@ -7,7 +7,7 @@ rfm_generate_data <- function(id_num=1000,
                               seed, ...) {
   date_type <- match.arg(date_type)
   if(!missing(seed)) set.seed(seed)
-  data <- data.frame(id=seq_len(id_num), count=rpois(id_num, lambda = 2))
+  data <- data_frame(id=seq_len(id_num), count=rpois(id_num, lambda = 2))
   ids <- apply(data, 1, function(x) rep(x[["id"]], x[["count"]])) %>% unlist
   n <- length(ids)
   payment <- round(rgamma(n, shape = 2, scale = 2) * 100) * 10
@@ -21,5 +21,5 @@ rfm_generate_data <- function(id_num=1000,
     date <- rfm_rdatetime(n, ...)
   }
   
-  data.frame(id=ids, payment=payment, date=date)
+  data_frame(id=ids, payment=payment, date=date)
 }
