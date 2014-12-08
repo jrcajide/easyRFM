de1a771a1badb269d49f2718aa6f220fca36b275
hoxo-m <hoxo.smile@gmail.com>
2014-12-09 00:14:01 +0900

    [modify] rfm_rdatetime()

--- R/rfm_rdatetime.R
+++ R/rfm_rdatetime.R
@@ -3,7 +3,8 @@
 #'
 #'@export
 rfm_rdatetime <- function(n, begin=as.POSIXct("2014-12-01", tz=Sys.timezone()), 
-                          end=as.POSIXct("2014-12-31", tz=Sys.timezone())) {
+                          end=as.POSIXct("2015-01-01", tz=Sys.timezone())) {
   datetime_seq <- seq(begin, end, by="sec")
+  datetime_seq <- datetime_seq[-length(datetime_seq)]
   sample(datetime_seq, size = n, replace = TRUE)
 }
