f9454b8d5ec5826adcd4f9288adb72b109f80e8d
hoxo-m <hoxo.smile@gmail.com>
2014-12-09 00:13:42 +0900

    [modify] rfm_rdate()

--- R/rfm_rdate.R
+++ R/rfm_rdate.R
@@ -2,10 +2,10 @@
 #'
 #'
 #'@export
-rfm_rdate <- function(n, begin=as.Date("2014-12-01"), end=as.Date("2014-12-31"),
-                      by="days") {
-  if(is.character(begin)) begin <- as.Date(begin)
-  if(is.character(end)) end <- as.Date(end)  
-  date_seq <- seq(begin, end, by=by)
+rfm_rdate <- function(n, begin=as.Date("2014-12-01"), end=as.Date("2015-01-01"),
+                      by="days", tz=Sys.timezone()) {
+  if(is.character(begin)) begin <- as.Date(begin, tz=tz)
+  if(is.character(end)) end <- as.Date(end, tz=tz)
+  date_seq <- seq(begin, end - 1, by=by)
   sample(date_seq, size = n, replace = TRUE)
 }
